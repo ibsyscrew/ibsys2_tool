d6bb48ac02a39ee460b83f0c8cf59cadc110865d
Matthias <Matthias@DELL>
2016-05-10 10:49:33 +0200

    neuster Stand

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Ausstehende_Bestellungen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace WindowsFormsApplication1
+{
+    class Ausstehende_Bestellungen //futureinwardstockmovement
+    {
+        public String orderperiod { set; get; }
+        public String id { set; get; } 
+        public String mode { set; get; } 
+        public String article { set; get; } 
+        public String amount { set; get; }
+
+        public Ausstehende_Bestellungen() { }
+        
+        public Ausstehende_Bestellungen(String op,String id, String mode, String article, String amount)
+        {
+            this.orderperiod = op;
+            this.id = id;
+            this.mode = mode;
+            this.article = article;
+            this.amount = amount;
+          }
+
+
+    }
+}
+
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Leerzeitenkosten.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Leerzeitenkosten //idletimecosts
+    {
+        public String workplace { set; get; }
+        public String id { set; get; } 
+        public String setupevents { set; get; }
+        public String idletime { set; get; } 
+        public String wageidletimecosts { set; get; }
+        public String wagecosts { set; get; }
+        public String machineidletimecosts { set; get; }
+
+        public Leerzeitenkosten() { }
+
+        public Leerzeitenkosten(String workplace, String id, String setupevents, String idletime,
+            String wageidletimecosts, String wagecosts, String machineidletimecosts)
+        {
+            this.workplace = workplace;
+            this.id = id;
+            this.setupevents = setupevents;
+            this.idletime = idletime;
+            this.wageidletimecosts = wageidletimecosts;
+            this.wagecosts = wagecosts;
+            this.machineidletimecosts = machineidletimecosts;
+                }
+
+
+    }
+}
+s
