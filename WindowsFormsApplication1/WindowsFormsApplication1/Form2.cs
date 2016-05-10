c42121cf29d19bc66bbc7b77cf1f20fa9657209d
Matthias <Matthias@DELL>
2016-05-10 12:11:41 +0200

    aaaa

--- WindowsFormsApplication1/WindowsFormsApplication1/Form2.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form2.cs
@@ -17,7 +17,6 @@ namespace WindowsFormsApplication1
         public Form2()
         {
             InitializeComponent();
-            deutsch();
             label1.Text = "Version: 1.5.2.1               MMJ$ Group Ltd.              "+ DateTime.Now.ToString();
         }
 
@@ -33,7 +32,7 @@ namespace WindowsFormsApplication1
                 xmldoc.Load(fs);
 
                 xmlnode = xmldoc.GetElementsByTagName("inwardstockmovement");
-                Database.bestellungen = new List<Bestellung>();
+                List<Bestellung> bestellungen = new List<Bestellung>();
                 for(int i = 0; i < xmlnode[0].ChildNodes.Count-1; i++)
                 {
                     //MessageBox.Show("Zeug: " + xmlnode[0].ChildNodes.Item(0).Attributes[0].Value.ToString());
@@ -48,23 +47,23 @@ namespace WindowsFormsApplication1
                         xmlnode[0].ChildNodes.Item(i).Attributes[8].Value.ToString(),
                         xmlnode[0].ChildNodes.Item(i).Attributes[9].Value.ToString()
                         );
-                    Database.bestellungen.Add(a);
+                    bestellungen.Add(a);
                 }
-                dataGridView2.DataSource = Database.bestellungen;
+                dataGridView2.DataSource = bestellungen;
                 xmlnode = xmldoc.GetElementsByTagName("warehousestock");
-                Database.lager = new List<Artikel>();
+                List<Warehousestock> lager = new List<Warehousestock>();
                 for (int i = 0; i < xmlnode[0].ChildNodes.Count - 1; i++)
                 {
                     //MessageBox.Show("Zeug: " + xmlnode[0].ChildNodes.Item(0).Attributes[0].Value.ToString());
-                    Artikel a = new Artikel(xmlnode[0].ChildNodes.Item(i).Attributes[0].Value.ToString(), xmlnode[0].ChildNodes.Item(i).Attributes[1].Value.ToString(), xmlnode[0].ChildNodes.Item(i).Attributes[2].Value.ToString(), xmlnode[0].ChildNodes.Item(i).Attributes[3].Value.ToString(), xmlnode[0].ChildNodes.Item(i).Attributes[4].Value.ToString(), xmlnode[0].ChildNodes.Item(i).Attributes[5].Value.ToString());
-                    Database.lager.Add(a);
+                    Warehousestock a = new Warehousestock(xmlnode[0].ChildNodes.Item(i).Attributes[0].Value.ToString(), xmlnode[0].ChildNodes.Item(i).Attributes[1].Value.ToString(), xmlnode[0].ChildNodes.Item(i).Attributes[2].Value.ToString(), xmlnode[0].ChildNodes.Item(i).Attributes[3].Value.ToString(), xmlnode[0].ChildNodes.Item(i).Attributes[4].Value.ToString(), xmlnode[0].ChildNodes.Item(i).Attributes[5].Value.ToString());
+                    lager.Add(a);
                 }
-                dataGridView1.DataSource = Database.lager;
+                dataGridView1.DataSource = lager;
                 label3.Text = "Gesamter Lagerwert: ";
                 label4.Text = xmlnode[0].ChildNodes.Item(xmlnode[0].ChildNodes.Count - 1).InnerText.ToString() + " €";
                 
                 xmlnode = xmldoc.GetElementsByTagName("futureinwardstockmovement");
-                Database.zukunftigeEingänge = new List<Ausstehende_Bestellungen>();
+                List<Ausstehende_Bestellungen> zukunftigeEingänge = new List<Ausstehende_Bestellungen>();
                 for (int i = 0; i < xmlnode[0].ChildNodes.Count - 1; i++)
                 {
                     //MessageBox.Show("Zeug: " + xmlnode[0].ChildNodes.Item(0).Attributes[0].Value.ToString());
@@ -74,26 +73,9 @@ namespace WindowsFormsApplication1
                         xmlnode[0].ChildNodes.Item(i).Attributes[2].Value.ToString(), 
                         xmlnode[0].ChildNodes.Item(i).Attributes[3].Value.ToString(), 
                         xmlnode[0].ChildNodes.Item(i).Attributes[4].Value.ToString()); ;
-                    Database.zukunftigeEingänge.Add(a);
+                    zukunftigeEingänge.Add(a);
                 }
-                dataGridView3.DataSource = Database.zukunftigeEingänge;
-
-                xmlnode = xmldoc.GetElementsByTagName("idletimecosts");
-                Database.strafkosten = new List<Leerzeitenkosten>();
-                for (int i = 0; i < xmlnode[0].ChildNodes.Count - 1; i++)
-                {
-                    //MessageBox.Show("Zeug: " + xmlnode[0].ChildNodes.Item(0).Attributes[0].Value.ToString());
-                    Leerzeitenkosten a = new Leerzeitenkosten(
-                        xmlnode[0].ChildNodes.Item(i).Attributes[0].Value.ToString(),
-                        xmlnode[0].ChildNodes.Item(i).Attributes[1].Value.ToString(),
-                        xmlnode[0].ChildNodes.Item(i).Attributes[2].Value.ToString(),
-                        xmlnode[0].ChildNodes.Item(i).Attributes[3].Value.ToString(),
-                        xmlnode[0].ChildNodes.Item(i).Attributes[4].Value.ToString(),
-                        xmlnode[0].ChildNodes.Item(i).Attributes[5].Value.ToString()
-                        ); ;
-                    Database.strafkosten.Add(a);
-                }
-                dataGridView4.DataSource = Database.strafkosten;
+                dataGridView3.DataSource = zukunftigeEingänge;
             }
         }
 
@@ -103,11 +85,6 @@ namespace WindowsFormsApplication1
         }
 
         private void deutschToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            deutsch();
-        }
-
-        public void deutsch()
         {
             tabPage1.Text = "Bestellungen";
             tabPage2.Text = "Lagerbestand";
@@ -120,20 +97,9 @@ namespace WindowsFormsApplication1
             tabPage7.Text = "Durchlaufzeiten";
             tabPage8.Text = "Ergebnisse";
             label3.Text = "Gesamter Lagerwert: ";
-            deutschToolStripMenuItem.Text = "Deutsch";
-            englischToolStripMenuItem.Text = "Englisch";
-            dateiToolStripMenuItem.Text = "Datei";
-            xMLImportierenToolStripMenuItem.Text = "Xml importieren";
-            xMLExportierenToolStripMenuItem.Text = "Xml exportieren";
-            spracheToolStripMenuItem.Text = "Sprache";
         }
 
         private void englischToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            englisch();
-        }
-
-        public void englisch()
         {
             tabPage1.Text = "Inward Stockmovement";
             tabPage2.Text = "Warehousestock";
@@ -146,12 +112,6 @@ namespace WindowsFormsApplication1
             tabPage7.Text = "Cycletimes";
             tabPage8.Text = "Results";
             label3.Text = "Totalstockvalue: ";
-            deutschToolStripMenuItem.Text = "German";
-            englischToolStripMenuItem.Text = "Englisch";
-            dateiToolStripMenuItem.Text = "Data";
-            spracheToolStripMenuItem.Text = "Language";
-            xMLImportierenToolStripMenuItem.Text = "Xml Import";
-            xMLExportierenToolStripMenuItem.Text = "Xml Export";
         }
     }
 }
